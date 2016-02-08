0f4ac78eebc39f8850818c2e0eb7d67aea0b8768
yba <yba@umich.edu>
2016-02-07 21:29:49 -0500

    YodaTester

--- QPMClient/QPMClient.java
+++ QPMClient/QPMClient.java
@@ -13,39 +13,42 @@ import org.apache.thrift.transport.TTransport;
 //Generated code
 import QPMstubs.QPM;
 
+/** 
+* A Yoda Client that sends a signle query to Yoda Server and prints the results.
+*/
 public class QPMClient {
 	
-  public static void main(String [] args) {
-  	// collect the port number and query
-  	int port = 8080;
+	public static void main(String [] args) {
+		// collect the port number and query
+		int port = 8080;
 
-    if (args.length >= 1) {
-      port = Integer.parseInt(args[0]);
-    }
+		if (args.length >= 1) {
+			port = Integer.parseInt(args[0]);
+		}
 
-    String query = "What is the speed of light?";
+		String query = "What is the speed of light?";
 
-    if (args.length >= 2) {
-      query = args[1];
-    }
-   
-    // initialize thrift objects
-    TTransport transport = new TSocket("localhost", port);
-    TProtocol protocol = new TBinaryProtocol(new TFramedTransport(transport));
-    QPM.Client client = new QPM.Client(protocol);
-    try {
-    	// talk to the parser
-      transport.open();
-      System.out.println("///// Connecting to Yoda... It might take a while for Yoda to find an answer. /////");
-      String answer = client.handleQuery(query);
-      
-      System.out.println("///// Answer: /////");
-      System.out.println(answer);
-      transport.close();
-    } catch (TException x) {
-      x.printStackTrace();
-    }
-    
-    return;
-  }
+		if (args.length >= 2) {
+			query = args[1];
+		}
+	 
+		// initialize thrift objects
+		TTransport transport = new TSocket("localhost", port);
+		TProtocol protocol = new TBinaryProtocol(new TFramedTransport(transport));
+		QPM.Client client = new QPM.Client(protocol);
+		try {
+			// talk to the parser
+			transport.open();
+			System.out.println("///// Connecting to Yoda... It might take a while for Yoda to find an answer. /////");
+			String answer = client.handleQuery(query);
+			
+			System.out.println("///// Answer: /////");
+			System.out.println(answer);
+			transport.close();
+		} catch (TException x) {
+			x.printStackTrace();
+		}
+		
+		return;
+	}
 }
